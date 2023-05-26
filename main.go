fe6eef67df276cfeb735b62c9a48f46c81023623
Ansar1to <ansarszdk@.com>
2023-05-26 11:31:07 +0600

    add main.go

--- main.go
+++ main.go
@@ -5,7 +5,31 @@ import (
  "fmt"
  "math"
  "math/cmplx"
- "math/rand"
+ "math/rand"go
+
+ package main
+ 
+ import "fmt"
+ 
+ // * pointer
+ // & adress
+ 
+ func changeLocal(num *[5]int) { // *
+   num[0] = 55
+ 
+   fmt.Println("\ninside function:\n ", &num)
+ 
+ }
+ func main() {
+   num := [...]int{5, 6, 7, 8, 8}
+   fmt.Println("\nbefore passing to function:\n ", &num)
+ 
+   changeLocal(&num) //num is passed by value
+   fmt.Println("\nadress of num:\n  ", &num)
+ 
+   fmt.Println("\nafter passing to function: ", &num)
+ }
+ 
 
 )
 
